7b541b5dcd6e51fd062473ba6652a2862c56e9a6
User <vjvimala123@gmail.com>
2026-03-30 10:25:47 +0530

    SiteSettings

---
+++ CateringApi/Controllers/SiteSettingsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CateringApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class SiteSettingsController : ControllerBase
+    {
+
+
+
+    }
+}
