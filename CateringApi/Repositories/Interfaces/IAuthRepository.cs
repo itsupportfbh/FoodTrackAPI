a0e88de01c2a822b415566247c867b86cf3cad93
Gowtham T <kuttygowtham756@gmail.com>
2026-03-31 10:08:39 +0530

    Login changes

---
+++ CateringApi/Repositories/Interfaces/IAuthRepository.cs
@@ -0,0 +1,9 @@
+﻿using CateringApi.DTOs.User;
+
+namespace CateringApi.Repositories.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<UserLoginDto?> GetUserByEmailAsync(string email);
+    }
+}
---
+++ CateringApi/Services/IJwtService.cs
@@ -0,0 +1,7 @@
+﻿namespace CateringApi.Services
+{
+    public interface IJwtService
+    {
+        string GenerateToken(int id, string username, string email, int roleId, int? companyId);
+    }
+}
