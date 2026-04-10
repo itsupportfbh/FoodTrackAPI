0fc3f3d8850d12c598b3e0e5920e3b4c45d93a3b
Gowtham T <kuttygowtham756@gmail.com>
2026-04-10 12:19:47 +0800

    User changes

---
+++ CateringApi/DTOs/CreateUserMasterDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CateringApi.DTOs
+{
+    public class CreateUserMasterDto
+    {
+        public int CompanyId { get; set; }
+        public int RoleId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+
+        public bool IsDelete { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+    }
+}
---
+++ CateringApi/DTOs/User/UserMasterDTO.cs
@@ -0,0 +1,23 @@
+﻿namespace CateringApi.DTOs.User
+{
+    public class UserMasterDTO
+    {
+        public int Id { get; set; } 
+        public int CompanyId { get; set; }
+        public int RoleId { get; set; }
+        public string UserName { get; set; }
+
+        public string Email {  get; set; }
+
+        public string PasswordHash { get; set; }
+        public bool IsActive { get; set; }
+
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public string CompanyName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
